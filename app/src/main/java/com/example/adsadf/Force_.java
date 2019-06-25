3fd5406c025adf8c00adea98eae4f22e33e0be78
Harsh Mahajan <harshmahajan927@gmail.com>
2019-06-25 17:42:33 +0530

    Fix missing id for linear layout

--- app/src/main/java/com/example/adsadf/Force_.java
+++ app/src/main/java/com/example/adsadf/Force_.java
@@ -1,22 +1,16 @@
 package com.example.adsadf;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import java.util.List;
 
@@ -26,83 +20,39 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Force_.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Force_#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Force_ extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    public ListView listView;
-    public List<Forces> forces;
-    public String[] forcename;
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private ListView listView;
+    private List<Forces> forces;
+    private String[] forcename;
 
     public Force_() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Force_.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Force_ newInstance(String param1, String param2) {
-        Force_ fragment = new Force_();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_force_, container, false);
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listView = getView().findViewById(R.id.listView);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_force_, container, false);
-        listView=view.findViewById(R.id.listview);
-
-        final Retrofit retrofit=new Retrofit.Builder().baseUrl(Api.BaseUrl).addConverterFactory(GsonConverterFactory.create()).build();
-        Api api=retrofit.create(Api.class);
-        Call<List<Forces>> call =api.getForces();
+        final Retrofit retrofit = new Retrofit.Builder().baseUrl(Api.BaseUrl).addConverterFactory(GsonConverterFactory.create()).build();
+        Api api = retrofit.create(Api.class);
+        Call<List<Forces>> call = api.getForces();
         call.enqueue(new Callback<List<Forces>>() {
             @Override
             public void onResponse(Call<List<Forces>> call, Response<List<Forces>> response) {
-                forces= response.body();
-                forcename= new String[forces.size()];
-                for(int i=0;i<forces.size();i++)
-                {
-                    forcename[i]=forces.get(i).getName();
-                    Log.d("anvv",forces.get(i).getName());
+                forces = response.body();
+                forcename = new String[forces.size()];
+                for (int i = 0; i < forces.size(); i++) {
+                    forcename[i] = forces.get(i).getName();
+                    Log.d(":::", forces.get(i).getName());
                 }
-                ArrayAdapter arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, forcename);
+                ArrayAdapter arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, forcename);
                 listView.setAdapter(arrayAdapter);
             }
 
@@ -111,74 +61,5 @@ public class Force_ extends Fragment {
 
             }
         });
-
-        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //Toast.makeText(this,forces.get(position).getId(),Toast.LENGTH_SHORT).show();
-                Log.e("touched",forces.get(position).getId());
-            }
-        });*/
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- app/src/main/java/com/example/adsadf/MainActivity.java
+++ app/src/main/java/com/example/adsadf/MainActivity.java
@@ -1,27 +1,12 @@
 package com.example.adsadf;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-public class MainActivity extends AppCompatActivity implements Force_.OnFragmentInteractionListener{
+public class MainActivity extends AppCompatActivity {
     public Force_ force_=new Force_();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +16,5 @@ public class MainActivity extends AppCompatActivity implements Force_.OnFragment
         FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.cont,force_);
         fragmentTransaction.commit();
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
     }
 }
